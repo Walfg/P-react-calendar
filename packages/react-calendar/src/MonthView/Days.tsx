f5ce740e9f1d18ff4e0b90baccfe4ead2ab37376
Walfg <35825287+Walfg@users.noreply.github.com>
2024-09-19 02:33:42 +0200

    Update Days.tsx

--- packages/react-calendar/src/MonthView/Days.tsx
+++ packages/react-calendar/src/MonthView/Days.tsx
@@ -1,123 +1,111 @@
+// Days.tsx
+
+import React, { useState, useEffect, FC } from 'react';
 import { getYear, getMonth, getDaysInMonth, getDayStart } from '@wojtekmaj/date-utils';
+import TileGroup from '../TileGroup';
+import Day from './Day';
+import { getDayOfWeek } from '../shared/dates';
 
-import TileGroup from '../TileGroup.js';
-import Day from './Day.js';
+interface DaysProps {
+    activeStartDate: Date;
+    calendarType: string;
+    hover?: boolean;
+    showFixedNumberOfWeeks?: boolean;
+    showNeighboringMonth?: boolean;
+    value?: Date | Date[] | null;
+    valueType?: string;
+    // Puedes extender esta interfaz con otros props si es necesario
+}
 
-import { getDayOfWeek } from '../shared/dates.js';
-
-import type { CalendarType } from '../shared/types.js';
-
-type DaysProps = {
-  /**
-   * The beginning of a period that shall be displayed.
-   *
-   * @example new Date(2017, 0, 1)
-   */
-  activeStartDate: Date;
-  /**
-   * Type of calendar that should be used. Can be `'gregory`, `'hebrew'`, `'islamic'`, `'iso8601'`. Setting to `"gregory"` or `"hebrew"` will change the first day of the week to Sunday. Setting to `"islamic"` will change the first day of the week to Saturday. Setting to `"islamic"` or `"hebrew"` will make weekends appear on Friday to Saturday.
-   *
-   * @example 'iso8601'
-   */
-  calendarType: CalendarType | undefined;
-  /**
-   * Whether to always show fixed number of weeks (6). Forces `showNeighboringMonth` prop to be `true`.
-   *
-   * @default false
-   * @example true
-   */
-  showFixedNumberOfWeeks?: boolean;
-  /**
-   * Whether days from previous or next month shall be rendered if the month doesn't start on the first day of the week or doesn't end on the last day of the week, respectively.
-   *
-   * @default true
-   * @example false
-   */
-  showNeighboringMonth?: boolean;
-} & Omit<
-  React.ComponentProps<typeof TileGroup>,
-  'dateTransform' | 'dateType' | 'end' | 'renderTile' | 'start'
-> &
-  Omit<React.ComponentProps<typeof Day>, 'classes' | 'currentMonthIndex' | 'date' | 'point'>;
-
-export default function Days(props: DaysProps): React.ReactElement {
-  const {
+const Days: FC<DaysProps> = ({
     activeStartDate,
     calendarType,
-    hover,
-    showFixedNumberOfWeeks,
-    showNeighboringMonth,
-    value,
-    valueType,
+    hover = false,
+    showFixedNumberOfWeeks = false,
+    showNeighboringMonth = false,
+    value = null,
+    valueType = 'day',
     ...otherProps
-  } = props;
+}) => {
+    const year = getYear(activeStartDate);
+    const monthIndex = getMonth(activeStartDate);
+    const hasFixedNumberOfWeeks = showFixedNumberOfWeeks || showNeighboringMonth;
+    const dayOfWeek = getDayOfWeek(activeStartDate, calendarType);
+    const offset = hasFixedNumberOfWeeks ? 0 : dayOfWeek;
+    const start = (hasFixedNumberOfWeeks ? -dayOfWeek : 0) + 1;
 
-  const year = getYear(activeStartDate);
-  const monthIndex = getMonth(activeStartDate);
+    const end = (() => {
+        if (showFixedNumberOfWeeks) {
+            return start + 6 * 7 - 1;
+        }
+        const daysInMonth = getDaysInMonth(activeStartDate);
+        if (showNeighboringMonth) {
+            const activeEndDate = new Date(activeStartDate);
+            activeEndDate.setDate(daysInMonth);
+            activeEndDate.setHours(0, 0, 0, 0);
+            const daysUntilEndOfTheWeek = 7 - getDayOfWeek(activeEndDate, calendarType) - 1;
+            return daysInMonth + daysUntilEndOfTheWeek;
+        }
+        return daysInMonth;
+    })();
 
-  const hasFixedNumberOfWeeks = showFixedNumberOfWeeks || showNeighboringMonth;
-  const dayOfWeek = getDayOfWeek(activeStartDate, calendarType);
+    // Estado para detectar si es un teléfono
+    const [isPhone, setIsPhone] = useState<boolean>(false);
 
-  const offset = hasFixedNumberOfWeeks ? 0 : dayOfWeek;
+    // Detección del dispositivo
+    useEffect(() => {
+        const detectDevice = () => {
+            const userAgent = navigator.userAgent || navigator.vendor || (window as any).opera;
+            let device = 'PC';
+            if (/android/i.test(userAgent)) {
+                device = 'Android';
+            } else if (/iPad|iPhone|iPod/.test(userAgent) && !(window as any).MSStream) {
+                device = 'iOS';
+            } else if (/windows phone/i.test(userAgent)) {
+                device = 'Windows Phone';
+            }
+            setIsPhone(device !== 'PC');
+        };
 
-  /**
-   * Defines on which day of the month the grid shall start. If we simply show current
-   * month, we obviously start on day one, but if showNeighboringMonth is set to
-   * true, we need to find the beginning of the week the first day of the month is in.
-   */
-  const start = (hasFixedNumberOfWeeks ? -dayOfWeek : 0) + 1;
+        detectDevice();
+    }, []);
 
-  /**
-   * Defines on which day of the month the grid shall end. If we simply show current
-   * month, we need to stop on the last day of the month, but if showNeighboringMonth
-   * is set to true, we need to find the end of the week the last day of the month is in.
-   */
-  const end = (() => {
-    if (showFixedNumberOfWeeks) {
-      // Always show 6 weeks
-      return start + 6 * 7 - 1;
-    }
+    // Asegurar que React no sea eliminado por el compilador
+    console.log(React.version);
 
-    const daysInMonth = getDaysInMonth(activeStartDate);
+    const className = isPhone
+        ? 'react-calendar__month-view__days-mobile'
+        : 'react-calendar__month-view__days';
 
-    if (showNeighboringMonth) {
-      const activeEndDate = new Date();
-      activeEndDate.setFullYear(year, monthIndex, daysInMonth);
-      activeEndDate.setHours(0, 0, 0, 0);
-      const daysUntilEndOfTheWeek = 7 - getDayOfWeek(activeEndDate, calendarType) - 1;
-      return daysInMonth + daysUntilEndOfTheWeek;
-    }
-
-    return daysInMonth;
-  })();
-
-  return (
-    <TileGroup
-      className="react-calendar__month-view__days"
-      count={7}
-      dateTransform={(day) => {
-        const date = new Date();
-        date.setFullYear(year, monthIndex, day);
-        return getDayStart(date);
-      }}
-      dateType="day"
-      hover={hover}
-      end={end}
-      renderTile={({ date, ...otherTileProps }) => (
-        <Day
-          key={date.getTime()}
-          {...otherProps}
-          {...otherTileProps}
-          activeStartDate={activeStartDate}
-          calendarType={calendarType}
-          currentMonthIndex={monthIndex}
-          date={date}
+    return (
+        <TileGroup
+            className={className}
+            count={7}
+            dateTransform={(day: number) => {
+                const date = new Date(activeStartDate);
+                date.setFullYear(year, monthIndex, day);
+                return getDayStart(date);
+            }}
+            dateType="day"
+            hover={hover}
+            end={end}
+            renderTile={({ date, ...otherTileProps }) => (
+                <Day
+                    {...otherProps}
+                    {...otherTileProps}
+                    activeStartDate={activeStartDate}
+                    calendarType={calendarType}
+                    currentMonthIndex={monthIndex}
+                    date={date}
+                />
+            )}
+            offset={offset}
+            start={start}
+            value={value}
+            valueType={valueType}
+            {...otherProps}
         />
-      )}
-      offset={offset}
-      start={start}
-      value={value}
-      valueType={valueType}
-    />
-  );
-}
+    );
+};
+
+export default Days;
