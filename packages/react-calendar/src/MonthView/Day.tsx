5a9ee9227e410202adbd979d6860c70b7487cae5
Walfg <35825287+Walfg@users.noreply.github.com>
2024-09-19 02:34:51 +0200

    Update Day.tsx

--- packages/react-calendar/src/MonthView/Day.tsx
+++ packages/react-calendar/src/MonthView/Day.tsx
@@ -1,81 +1,60 @@
+// Day.tsx
+
+import React, { FC } from 'react';
 import { getDayStart, getDayEnd } from '@wojtekmaj/date-utils';
-
-import Tile from '../Tile.js';
-
-import { isWeekend } from '../shared/dates.js';
+import Tile from '../Tile';
+import { isWeekend } from '../shared/dates';
 import {
-  formatDay as defaultFormatDay,
-  formatLongDate as defaultFormatLongDate,
-} from '../shared/dateFormatter.js';
+    formatDay as defaultFormatDay,
+    formatLongDate as defaultFormatLongDate,
+} from '../shared/dateFormatter';
 
-import type { CalendarType } from '../shared/types.js';
-
-const className = 'react-calendar__month-view__days__day';
-
-type DayProps = {
-  /**
-   * Type of calendar that should be used. Can be `'gregory`, `'hebrew'`, `'islamic'`, `'iso8601'`. Setting to `"gregory"` or `"hebrew"` will change the first day of the week to Sunday. Setting to `"islamic"` will change the first day of the week to Saturday. Setting to `"islamic"` or `"hebrew"` will make weekends appear on Friday to Saturday.
-   *
-   * @example 'iso8601'
-   */
-  calendarType: CalendarType | undefined;
-  classes?: string[];
-  currentMonthIndex: number;
-  /**
-   * Function called to override default formatting of day tile labels. Can be used to use your own formatting function.
-   *
-   * @example (locale, date) => formatDate(date, 'd')
-   */
-  formatDay?: typeof defaultFormatDay;
-  /**
-   * Function called to override default formatting of day tile `abbr` labels. Can be used to use your own formatting function.
-   *
-   * @example (locale, date) => formatDate(date, 'dd MMM YYYY')
-   */
-  formatLongDate?: typeof defaultFormatLongDate;
-} & Omit<
-  React.ComponentProps<typeof Tile>,
-  'children' | 'formatAbbr' | 'maxDateTransform' | 'minDateTransform' | 'view'
->;
-
-export default function Day({
-  calendarType,
-  classes = [],
-  currentMonthIndex,
-  formatDay = defaultFormatDay,
-  formatLongDate = defaultFormatLongDate,
-  ...otherProps
-}: DayProps): React.ReactElement {
-  const { date, locale } = otherProps;
-
-  const classesProps: string[] = [];
-
-  if (classes) {
-    classesProps.push(...classes);
-  }
-
-  if (className) {
-    classesProps.push(className);
-  }
-
-  if (isWeekend(date, calendarType)) {
-    classesProps.push(`${className}--weekend`);
-  }
-
-  if (date.getMonth() !== currentMonthIndex) {
-    classesProps.push(`${className}--neighboringMonth`);
-  }
-
-  return (
-    <Tile
-      {...otherProps}
-      classes={classesProps}
-      formatAbbr={formatLongDate}
-      maxDateTransform={getDayEnd}
-      minDateTransform={getDayStart}
-      view="month"
-    >
-      {formatDay(locale, date)}
-    </Tile>
-  );
+interface DayProps {
+    calendarType: string;
+    classes?: string[];
+    currentMonthIndex: number;
+    formatDay?: (locale: string, date: Date) => string;
+    formatLongDate?: (locale: string, date: Date) => string;
+    date: Date;
+    locale: string;
 }
+
+const Day: FC<DayProps> = ({
+    calendarType,
+    classes = [],
+    currentMonthIndex,
+    formatDay = defaultFormatDay,
+    formatLongDate = defaultFormatLongDate,
+    date,
+    locale,
+    ...otherProps
+}) => {
+    const isMobile = typeof navigator !== 'undefined' && /Mobi|Android/i.test(navigator.userAgent);
+    const baseClassName = isMobile
+        ? 'react-calendar__month-view__days__day-mobile'
+        : 'react-calendar__month-view__days__day';
+
+    const classesProps: string[] = [...classes, baseClassName];
+
+    if (isWeekend(date, calendarType)) {
+        classesProps.push(`${baseClassName}--weekend`);
+    }
+    if (date.getMonth() !== currentMonthIndex) {
+        classesProps.push(`${baseClassName}--neighboringMonth`);
+    }
+
+    return (
+        <Tile
+            {...otherProps}
+            classes={classesProps}
+            formatAbbr={formatLongDate}
+            maxDateTransform={getDayEnd}
+            minDateTransform={getDayStart}
+            view="month"
+        >
+            {formatDay(locale, date)}
+        </Tile>
+    );
+};
+
+export default Day;
